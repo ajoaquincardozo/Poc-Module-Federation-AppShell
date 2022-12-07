b09dc00adcb75e0cdd454f0fdad27dbbe636be33
Alexander Joaquin Cardozo <a.joaquincardozo@gmail.com>
2022-12-07 08:35:42 -0300

    Se agrega evento p/ escuchar add-product del mfe

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 
 import Products from "home/Products";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Products/>
-  </div>
-);
+const App = () => {
+  const handleAddProduct = (event) => {
+    const { id, price, quantity } = event.detail.newProduct;
+    console.log("Shell-id", id);
+    console.log("Shell-price", price);
+    console.log("Shell-quantity", quantity);
+  };
+
+  useEffect(() => {
+    window.addEventListener('add-product', handleAddProduct);
+
+    // cleanup this component
+    return () => {
+      window.removeEventListener('add-product', handleAddProduct);
+    };
+  }, []);
+
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <Products/>
+    </div>
+  )
+};
 ReactDOM.render(<App />, document.getElementById("app"));
