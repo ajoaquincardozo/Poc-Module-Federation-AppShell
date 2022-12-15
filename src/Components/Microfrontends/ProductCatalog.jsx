37f8a2d846926424638d828a24ffe407558f1ff6
Alexander Joaquin Cardozo <a.joaquincardozo@gmail.com>
2022-12-15 15:09:41 -0300

    detalle de producto (a partir de mfe) y navegacion

--- src/Components/Microfrontends/ProductCatalog.jsx
+++ src/Components/Microfrontends/ProductCatalog.jsx
@@ -1,30 +1,45 @@
 // import React, { useState, useEffect, Suspense } from "react";
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 const ProductsMfe = React.lazy(() => import("mfeProducts/Products"));
 
 export default function ProductCatalog() {
-    // const [showProducts, setShowProducts] = useState(true);
+  const navigate = useNavigate();
+  // const [showProducts, setShowProducts] = useState(true);
 
-    const handleAddProduct = (event) => {
-      const newProduct = event.detail.newProduct;
-      alert(JSON.stringify(newProduct, null, 2));
+  const handleAddProduct = (event) => {
+    const newProduct = event.detail.newProduct;
+    alert(JSON.stringify(newProduct, null, 2));
+  };
+
+  useEffect(() => {
+    window.addEventListener('add-product', handleAddProduct);
+
+    // cleanup this component
+    return () => {
+      window.removeEventListener('add-product', handleAddProduct);
     };
+  }, []);
 
-    useEffect(() => {
-      window.addEventListener('add-product', handleAddProduct);
+  useEffect(() => {
+    window.addEventListener('add-product', handleAddProduct);
 
-      // cleanup this component
-      return () => {
-        window.removeEventListener('add-product', handleAddProduct);
-      };
-    }, []);
+    // cleanup this component
+    return () => {
+      window.removeEventListener('add-product', handleAddProduct);
+    };
+  }, []);
 
-    return (
-        <>
-            {/* <button className="bg-gray-800 text-white font-bold py-2 px-4 rounded text-xl mb-5 hidden" onClick={() => setShowProducts(!showProducts)}>Mostrar</button> */}
-            <React.Suspense fallback={<div>Cargando productos....</div>}>
-              <ProductsMfe/>
-            </React.Suspense>
-        </>
-    );
+  const onDetailClick = useCallback((id) => {
+    navigate(`/${id}`);
+  }, []);
+
+  return (
+      <>
+        {/* <button className="bg-gray-800 text-white font-bold py-2 px-4 rounded text-xl mb-5 hidden" onClick={() => setShowProducts(!showProducts)}>Mostrar</button> */}
+        <React.Suspense fallback={<div>Cargando productos....</div>}>
+          <ProductsMfe onDetailClick={onDetailClick}/>
+        </React.Suspense>
+      </>
+  );
 }
